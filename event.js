00bab89e3751b73691da21f682ed83635c075090
Bend <144515341+APIDT@users.noreply.github.com>
2024-04-13 09:43:20 +0300

    Update event.js

--- event.js
+++ event.js
@@ -4,29 +4,35 @@ const EventEmitter = require('events')
 const eventEmitter = new EventEmitter()
 
 function readTextFile(fileRead) {
-    fs.readFile(fileRead, 'utf8', (err, success) => {
-        if (err) {
-            eventEmitter.emit('error', err)
-            return
-        }
-        
-        let modifiedText = '';
-        for (let i = 0; i < success.length; i++) {
-            if ((i + 1) % 3 === 0) {
-                modifiedText += success[i].toUpperCase();
+    const stream = fs.createReadStream(fileRead, { encoding: 'utf8' })
+
+    let modifiedText = ''
+    let position = 0
+
+    stream.on('data', (chunk) => {
+        for (let i = 0; i < chunk.length; i++) {
+            if ((position + i + 1) % 3 === 0) {
+                modifiedText += chunk[i].toUpperCase()
             } else {
-                modifiedText += success[i];
+                modifiedText += chunk[i]
             }
         }
+        position += chunk.length
+    });
 
+    stream.on('end', () => {
         eventEmitter.emit('fileRead', modifiedText)
-    })
+    });
+
+    stream.on('error', (err) => {
+        eventEmitter.emit('error', err)
+    });
 }
 
-const fileRead = 'text.text'
+const fileRead = 'text.txt'
 
 eventEmitter.on('fileRead', (success) => {
-    console.log('File content successfully:', success);
+    console.log('File content successfully:', success)
 });
 
 eventEmitter.on('error', (err) => {
