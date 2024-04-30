8eb8b4eccde1b96840338d81c3c82e4933dbe7b2
Bend <144515341+APIDT@users.noreply.github.com>
2024-04-30 10:54:25 +0300

    Update stream.js

--- event.js
+++ event.js
@@ -1,42 +1,44 @@
-const fs = require('fs')
-const EventEmitter = require('events')
+const fs = require('fs');
+const EventEmitter = require('events');
 
-const eventEmitter = new EventEmitter()
+const eventEmitter = new EventEmitter();
 
 function readTextFile(fileRead) {
-    const stream = fs.createReadStream(fileRead, { encoding: 'utf8' })
+    const stream = fs.createReadStream(fileRead, { encoding: 'utf8' });
 
-    let modifiedText = ''
-    let position = 0
+    let modifiedText = '';
+    let position = 0;
 
     stream.on('data', (chunk) => {
+        let chunkText = '';
         for (let i = 0; i < chunk.length; i++) {
             if ((position + i + 1) % 3 === 0) {
-                modifiedText += chunk[i].toUpperCase()
+                chunkText += chunk[i].toUpperCase();
             } else {
-                modifiedText += chunk[i]
+                chunkText += chunk[i];
             }
         }
-        position += chunk.length
+        modifiedText += chunkText;
+        position += chunk.length;
     });
 
     stream.on('end', () => {
-        eventEmitter.emit('fileRead', modifiedText)
+        eventEmitter.emit('fileRead', modifiedText);
     });
 
     stream.on('error', (err) => {
-        eventEmitter.emit('error', err)
+        eventEmitter.emit('error', err);
     });
 }
 
-const fileRead = 'text.txt'
+const fileRead = 'text.txt';
 
 eventEmitter.on('fileRead', (success) => {
-    console.log('File content successfully:', success)
+    console.log('File content successfully:', success);
 });
 
 eventEmitter.on('error', (err) => {
-    console.error('Error occurred:', err)
-})
+    console.error('Error occurred:', err);
+});
 
-readTextFile(fileRead)
+readTextFile(fileRead);
